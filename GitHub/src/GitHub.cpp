5f8a82de417324c92130f717d03e3388b8d125d2
Roger Sarmento <rogerms@ifce.edu.br>
2020-05-08 09:19:46 -0300

    Add arquivos via upload

---
+++ GitHub/src/GitHub.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : GitHub.cpp
+// Author      : Roger M Sarmento
+// Version     :
+// Copyright   : © 2020 SARMENTO R. M. All Rights Reserved.
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "-c -fmessage-length=0  -std=c++11" << endl; // prints -c -fmessage-length=0  -std=c++11
+	return 0;
+}
